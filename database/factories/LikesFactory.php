cc5c15e55bdf8747b39af1eaff73309b144e0dab
Joshuashibu <joshua.wittypen@gmail.com>
2019-10-20 12:22:50 +0530

    intial changes

---
+++ database/factories/LikesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Likes;
+use Faker\Generator as Faker;
+
+$factory->define(Likes::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Reply;
+use Faker\Generator as Faker;
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
